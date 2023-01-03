d942d713a535a95ed3cb11b8ed939c13deacf2ed
steeveeh <steven.harwood@microsoft.comh>
2023-01-03 18:50:25 +0000

    moved it back to normal homepage

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('testing.html')
+   return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
